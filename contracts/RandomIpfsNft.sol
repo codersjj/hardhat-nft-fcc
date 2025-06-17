9b2f1e9be0fea13e50ae46e40f90cf2b885b9ed9
codersjj <shanewestlife@outlook.com>
2025-06-17 17:21:47 +0800

    feat: Random IPFS NFT-Creating Rare NFTs

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -5,6 +5,8 @@ import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFCo
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+error RandomIpfsNft__RangeOutOfBounds();
+
 contract RandomIpfsNft is VRFConsumerBaseV2Plus, ERC721 {
     // when we mint an NFT, we will trigger a Chainlink VRF call to get us a random number
     // using that number, we will get a random NFT
@@ -14,6 +16,13 @@ contract RandomIpfsNft is VRFConsumerBaseV2Plus, ERC721 {
     // St. Bernard common
     // users have to pay to mint an NFT
     // the owner of the contract can withdraw the ETH
+
+    // Type Declaration
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
 
     // Chainlink VRF Variables
     uint256 private immutable i_subscriptionId;
@@ -28,6 +37,7 @@ contract RandomIpfsNft is VRFConsumerBaseV2Plus, ERC721 {
 
     // NFT Variables
     uint256 public s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
 
     constructor(
         address vrfCoordinator,
@@ -66,7 +76,40 @@ contract RandomIpfsNft is VRFConsumerBaseV2Plus, ERC721 {
         uint256[] calldata randomWords
     ) internal override {
         address dogOwner = s_requestIdToSender[requestId];
+
+        // What does this token look like?
+        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
+        // 0-99
+        // 7 -> PUG
+        // 12 -> Shiba Inu
+        // 88 -> St. Bernard
+        // 45 -> St. Bernard
+
+        Breed dogBreed = getBreedFromModdedRng(moddedRng);
         _safeMint(dogOwner, s_tokenCounter);
+    }
+
+    function getBreedFromModdedRng(
+        uint256 moddedRng
+    ) public pure returns (Breed) {
+        uint256[3] memory chanceArray = getChanceArray();
+        uint256 cumulativeSum = 0;
+        // for example: moddedRng = 25
+        for (uint256 i = 0; i < chanceArray.length; i++) {
+            if (
+                moddedRng >= cumulativeSum &&
+                moddedRng < cumulativeSum + chanceArray[i]
+            ) {
+                return Breed(i);
+            }
+            cumulativeSum += chanceArray[i];
+        }
+
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns (uint256[3] memory) {
+        return [10, 30, MAX_CHANCE_VALUE];
     }
 
     function tokenURI(uint256) public view override returns (string memory) {}
