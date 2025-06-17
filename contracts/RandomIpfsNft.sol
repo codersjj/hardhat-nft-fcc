97acc03997244dff11e1fede91fc7b9369477d62
codersjj <shanewestlife@outlook.com>
2025-06-17 16:17:25 +0800

    feat: Random IPFS NFT-requestRandomWords

---
+++ contracts/RandomIpfsNft.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract RandomIpfsNft is VRFConsumerBaseV2Plus {
+    // when we mint an NFT, we will trigger a Chainlink VRF call to get us a random number
+    // using that number, we will get a random NFT
+    // Pug, Shiba Inu, St. Bernard
+    // Pug super rare
+    // Shiba Inu kind of rare
+    // St. Bernard common
+    // users have to pay to mint an NFT
+    // the owner of the contract can withdraw the ETH
+
+    // Chainlink VRF Variables
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    bool private s_enableNativePayment;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    constructor(
+        address vrfCoordinator,
+        uint256 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit,
+        bool enableNativePayment
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
+        s_enableNativePayment = enableNativePayment;
+    }
+
+    function requestNft() public returns (uint256 requestId) {
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_gasLane,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: s_enableNativePayment
+                    })
+                )
+            })
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {}
+
+    function setEnableNativePayment(bool enableNativePayment) external {
+        s_enableNativePayment = enableNativePayment;
+    }
+
+    function getEnableNativePayment() public view returns (bool) {
+        return s_enableNativePayment;
+    }
+}
