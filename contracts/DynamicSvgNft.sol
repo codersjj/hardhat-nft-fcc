521fd8d9271d109ae867903a9db6da2e35a05733
codersjj <shanewestlife@outlook.com>
2025-06-25 14:55:20 +0800

    feat: Dynamic SVG On-Chain NFT Base64 Encoding

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     // mint
@@ -11,12 +12,28 @@ contract DynamicSvgNft is ERC721 {
     // Some logic to say "Show X image" or "Show Y image"
 
     uint256 private s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant BASE64_ENCODED_SVG_PREFIX =
+        "data:image/svg+xml;base64,";
 
     constructor(
         string memory lowSvg,
         string memory highSvg
     ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return
+            string(
+                abi.encodePacked(BASE64_ENCODED_SVG_PREFIX, svgBase64Encoded)
+            );
     }
 
     function mintNft() public {
