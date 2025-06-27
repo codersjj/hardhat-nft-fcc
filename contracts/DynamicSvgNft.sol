d499ca07c04f72a50160587765a05606dfe47231
codersjj <shanewestlife@outlook.com>
2025-06-27 17:32:43 +0800

    feat: Advanced: EVM Opcodes, Encoding, and Calling Creating an NFT TokenURI on-Chain

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -6,6 +6,8 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "base64-sol/base64.sol";
 
+error ERC721Metadata__URI_QueryFor_NonExistentToken();
+
 contract DynamicSvgNft is ERC721 {
     // mint
     // store our SVG information somewhere
@@ -27,6 +29,8 @@ contract DynamicSvgNft is ERC721 {
     function svgToImageURI(
         string memory svg
     ) public pure returns (string memory) {
+        // example:
+        // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
         string memory svgBase64Encoded = Base64.encode(
             bytes(string(abi.encodePacked(svg)))
         );
@@ -39,5 +43,41 @@ contract DynamicSvgNft is ERC721 {
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        if (_ownerOf(tokenId) == address(0)) {
+            revert ERC721Metadata__URI_QueryFor_NonExistentToken();
+        }
+
+        string memory imageURI = "hi!";
+
+        // https://developer.mozilla.org/en-US/docs/Web/URI/Reference/Schemes/data
+        // image URI: data:image/svg+xml;base64,<data>
+        // JSON URI: data:application/json;base64,<data>
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"An NFT that changes based on the Chainlink Feed", "attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
